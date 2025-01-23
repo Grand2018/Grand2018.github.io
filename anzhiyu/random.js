74ff4d3bb7910dde080f75cb67829de94ca20e66
Grand2018 <272123092@qq.com>
2025-01-23 10:11:16 +0800

    Site updated: 2025-01-23 10:11:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/16/cloudflare R2/","2025/01/08/openclash/"];function toRandomPost(){
+var posts=["2025/01/16/cloudflare R2/","2025/01/16/tiktok android/","2025/01/16/uspto00/","2025/01/16/usview/","2025/01/08/openclash/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
